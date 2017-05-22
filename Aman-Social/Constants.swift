3899b73267493419ffd6338d25b79d1e3b3b8738
Aman Kumar <amankumar@Amans-MacBook-Air.local>
2017-05-23 01:42:28 +0530

    testing the github,So started with origin from email-auth

---
+++ Aman-Social/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Aman-Social
+//
+//  Created by Aman Kumar on 5/17/17.
+//  Copyright © 2017 Aman Kumar. All rights reserved.
+//
+
+import Foundation
---
+++ Aman-Social/FancyBtn.swift
@@ -0,0 +1,21 @@
+//
+//  FancyBtn.swift
+//  Aman-Social
+//
+//  Created by Aman Kumar on 5/22/17.
+//  Copyright © 2017 Aman Kumar. All rights reserved.
+//
+
+import UIKit
+
+class FancyBtn: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
